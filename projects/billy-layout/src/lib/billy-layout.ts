9d1ae03a4e6897ad1a7603def2c8129f7ef0ed93
Adrien Dessilly <adessilly@gmail.com>
2026-07-17 16:51:19 +0200

    initial commit

---
+++ projects/billy-layout/src/lib/billy-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-billy-layout',
+  imports: [],
+  template: ` <p>billy-layout works!</p> `,
+  styles: ``,
+})
+export class BillyLayout {}
---
+++ projects/billy-layout/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of billy-layout
+ */
+
+export * from './lib/billy-layout';
